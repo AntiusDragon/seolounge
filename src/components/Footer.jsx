f7db10004b893dccec7f7b41946a70337ec23bd2
AntiusDragon <145384189+AntiusDragon@users.noreply.github.com>
2023-11-20 20:29:03 +0200

    Router

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,5 @@
+import { FaTwitter, FaInstagram, FaFacebook } from 'react-icons/fa';
+
 export function Footer() {
     return (
         <div className="container">
@@ -52,9 +54,24 @@ export function Footer() {
             <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
               <p>© 2023 Company, Inc. All rights reserved.</p>
               <ul className="list-unstyled d-flex">
-                <li className="ms-3"><a className="link-body-emphasis" href="/">TW<svg className="bi" width="24" height="24"></svg></a></li>
-                <li className="ms-3"><a className="link-body-emphasis" href="/">IG<svg className="bi" width="24" height="24"></svg></a></li>
-                <li className="ms-3"><a className="link-body-emphasis" href="/">FB<svg className="bi" width="24" height="24"></svg></a></li>
+
+                <li className="ms-3">
+                    <a className="link-body-emphasis" href="/">
+                        <FaTwitter />
+                    </a>
+                </li>
+
+                <li className="ms-3">
+                    <a className="link-body-emphasis" href="/">
+                        <FaInstagram />
+                    </a>
+                </li>
+
+                <li className="ms-3">
+                    <a className="link-body-emphasis" href="/">
+                        <FaFacebook />
+                    </a>
+                </li>
               </ul>
             </div>
           </footer>
